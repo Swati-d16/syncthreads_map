c7ba4d567653067f23a347cd48bedf0eb78d6117
Swati-d16 <swati.d005@gmail.com>
2025-03-24 17:01:49 +0530

    upload

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 // Connect to MongoDB Atlas
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((error) => console.error(" MongoDB Connection Error:", error));
 
