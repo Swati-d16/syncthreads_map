68225415e66301a5d5dc6564215e588a0bfc6212
Swati-d16 <swati.d005@gmail.com>
2025-03-24 22:52:46 +0530

    up

--- server/server.js
+++ server/server.js
@@ -56,7 +56,8 @@ mongoose.connection.once("open", addDefaultUser);
 app.use("/api", require("./routes/authRoutes"));
 app.use("/api", require("./routes/dashboardRoutes"));
 
-module.exports = app;
-
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(` Server running at http://localhost:${PORT}`));
 
 
